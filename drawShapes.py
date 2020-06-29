6797f6c47e27431c119fa6c5045c21746db4d9d0
abhinav-TB <abhinavtb@gmail.com>
2020-06-29 21:07:34 +0530

    shapes drawn

--- drawShapes.py
+++ drawShapes.py
@@ -1,7 +1,20 @@
 import numpy as np 
 import cv2
+# color in blue green red format
 
-img =cv2.imread('./data/lena.jpg',0)
+# geting image from disk
+# img =cv2.imread('./data/lena.jpg',1)
+ 
+#  getting image using numpy
+img=np.zeros([512,512,3],np.uint8) 
+
+
+img=cv2.line(img,(0,0),(255,255),(55,45,35),4)
+img=cv2.arrowedLine(img,(0,0),(255,45 ),(55,45,35),4)
+img=cv2.rectangle(img,(384,0),(510,128),(0,0,255),10)
+img=cv2.circle(img,(447,63),63,(0,255,0),-1)
+font=cv2.FONT_HERSHEY_SIMPLEX
+img=cv2.putText(img,'opencv',(10,500),font,4,(255,255,255),10,cv2.LINE_AA)
 
 cv2.imshow ('imgage',img)
 cv2.waitKey(0)
