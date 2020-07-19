b5b31165ff87523a4906075d23a0ef143fad8f53
abhinav-TB <abhinavtb@gmail.com>
2020-07-19 23:02:11 +0530

    messi ball copied😎

--- region of interest/copying a object in a pic.py
+++ region of interest/copying a object in a pic.py
@@ -1,3 +1,11 @@
+# there is picture of messi hitting a ball and thar ball is copied to the side
+
 import numpy as np
 import cv2
-img-cv2.imread('./data/messi.jpg')
+img=cv2.imread('./data/messi5.jpg')
+print(img.shape)
+ball=img[280:340,330:390]
+img[273:333,100:160]=ball
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
