c6a670b9827151f22e6f92e4553fb98d03b9c44f
abhinav-TB <abhinavtb@gmail.com>
2020-07-19 23:17:55 +0530

    water mark🤩

---
+++ region of interest/add two pictures.py
@@ -0,0 +1,19 @@
+# this snippet is used to metge two images in add method 
+# addWeight method is used to add weight especially to a particular image this is used in watermarl technology
+import numpy as np
+import cv2
+img=cv2.imread('./data/messi5.jpg')
+img2=cv2.imread('./data/opencv-logo.png')
+print(img.shape)
+
+ball=img[280:340,330:390]
+img[273:333,100:160]=ball
+img=cv2.resize(img,(512,512))
+img2=cv2.resize(img2,(512,512))
+
+# dst=cv2.add(img,img2)
+dst=cv2.addWeighted(img,.9,img2,.1,0)
+
+cv2.imshow('image',dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
